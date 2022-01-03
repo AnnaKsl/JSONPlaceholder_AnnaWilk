24c03a9faf9d7bc50a02bd5fa753bd83b2729e78
AnnaKsl <kislaya.ann@gmail.com>
2022-01-03 13:26:39 +0100

    first 2022 commit

--- src/main/java/steps/AlbumSteps.java
+++ src/main/java/steps/AlbumSteps.java
@@ -5,9 +5,10 @@ import org.assertj.core.api.Assertions;
 import org.assertj.core.api.SoftAssertions;
 
 public class AlbumSteps {
-    SoftAssertions softly = new SoftAssertions();
+
 
     public void verifyAlbum(Album album, int id, int userid, String title) {
+        var softly = new SoftAssertions();
         softly.assertThat(album.getId()).isEqualTo(id);
         softly.assertThat(album.getUserId()).isEqualTo(userid);
         softly.assertThat(album.getTitle()).isEqualTo(title);
--- src/main/java/steps/CommentSteps.java
+++ src/main/java/steps/CommentSteps.java
@@ -5,14 +5,14 @@ import org.assertj.core.api.Assertions;
 import org.assertj.core.api.SoftAssertions;
 
 public class CommentSteps {
-    SoftAssertions softly = new SoftAssertions();
+
 
     public void verifyComment(Comment comment, int postId, String name, String email, String body) {
-
+        var softly = new SoftAssertions();
         softly.assertThat(comment.getPostId()).isEqualTo(postId);
-        softly.assertThat(comment.getName()).isEqualTo(name);
-        softly.assertThat(comment.getEmail()).isEqualTo(email);
-        softly.assertThat(comment.getBody()).isEqualTo(body);
+        softly.assertThat(comment.getName()).as("Name is not as expected").isEqualTo(name);
+        softly.assertThat(comment.getEmail()).as("Email is not as expected").isEqualTo(email);
+        softly.assertThat(comment.getBody()).as("Body is not as expected").isEqualTo(body);
         softly.assertAll();
     }
 
